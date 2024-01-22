9a39f53d39d1bdd94b71f2e5630f0575cb3b2b9a
seba17962 <isebsebastiansilva@gmail.com>
2024-01-22 12:51:56 +0000

    task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+if (process.argv.length !== 3) {
+  console.log("Usage: node script.js <file_path>");
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (error, content) => {
+  if (error) {
+      console.error("Error during the reading:", error);
+  } else {
+      console.log("content at the file:", content);
+  }
+});
