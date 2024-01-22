f3ba0b0aaf936bbaf8100b04b517f0b2f2e1711c
seba17962 <isebsebastiansilva@gmail.com>
2024-01-22 15:52:21 +0000

    task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,16 +2,16 @@
 const request = require('request');
 
 if (process.argv.length !== 3) {
-    console.log("Usage: node script.js <URL>");
-    process.exit(1);
+  console.log("Usage: node script.js <URL>");
+  process.exit(1);
 }
 
 const url = process.argv[2];
 
-request.get(url, (error, response) => {
-    if (error) {
-        console.error("Error:", error.message);
-    } else {
-        console.log(`code: ${response.statusCode}`);
-    }
+request(url, (error, response) => {
+  if (error) {
+    console.error("Error:", error.message);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
