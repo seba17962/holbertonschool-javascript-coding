55db4a8284c6f9f7635940d4fde6c5f28dd6e462
seba17962 <isebsebastiansilva@gmail.com>
2024-01-23 14:54:25 +0000

    task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.log('Usage: node script.js movie_ID');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const apiURL = 'https://swapi-api.hbtn.io/api/films/';
+
+request.get(`${apiURL}${movieId}`, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(response.data);
+  }
+})
