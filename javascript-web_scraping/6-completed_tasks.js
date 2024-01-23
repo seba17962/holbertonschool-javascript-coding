6328e706e15e140c4d32a40dfa381dfaa970b9c3
seba17962 <isebsebastiansilva@gmail.com>
2024-01-23 15:43:43 +0000

    task 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,24 +1,27 @@
 #!/usr/bin/node
-
 const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.log('Usage: node script.js <API_URL>');
+  process.exit(1);
+}
 
 const apiUrl = process.argv[2];
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
+  } else {
+    const resDic = {};
+    const data = JSON.parse(body);
+    data.forEach((task) => {
+      if (task.completed) {
+        typeof resDic[task.userId] === 'undefined'
+          ? (resDic[task.userId] = 1)
+          : (resDic[task.userId] += 1);
+      }
+    });
+
+    console.log(resDic);
   }
-
-  const resDic = {};
-  const data = JSON.parse(body);
-
-  data.forEach((task) => {
-    if (task.completed) {
-      typeof resDic[task.userId] === 'undefined'
-        ? (resDic[task.userId] = 1)
-        : (resDic[task.userId] += 1);
-    }
-  });
-
-  console.log(resDic);
 });
