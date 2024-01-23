a88fe12acba9ffe8fbdfbf964bc79257211bdff4
seba17962 <isebsebastiansilva@gmail.com>
2024-01-23 15:30:17 +0000

    task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length !== 4) {
+  console.log('Usage: node script.js <API_URL> FilePath');
+  process.exit(1);
+}
+
+const api_url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(api_url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+
+    fs.writeFileSync(filePath, body, 'utf-8', (error) => {
+      if (error) {
+        console.error(error);
+      }
+    });
+  }
+});
